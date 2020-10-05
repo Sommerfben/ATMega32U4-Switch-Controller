73641dc4ebb86c51b5b849cd3874fe20b4841e57
unknown <sommerfben@gmail.com>
2020-10-05 15:34:42 -0700

    first commit

---
+++ Switch_controller_rx/controller_rx/controller_rx.ino
@@ -0,0 +1,372 @@
+
+//THIS MUST BE OPEN IN ARDUINO VER 1.8.2
+
+/*
+
+Demonstrates simple RX and TX operation.
+Any of the Basic_TX examples can be used as a transmitter.
+Please read through 'NRFLite.h' for a description of all the methods available in the library.
+
+Radio    Arduino
+CE    -> 9
+CSN   -> 10 (Hardware SPI SS)
+MOSI  -> 11 (Hardware SPI MOSI)
+MISO  -> 12 (Hardware SPI MISO)
+SCK   -> 13 (Hardware SPI SCK)
+IRQ   -> No connection
+VCC   -> No more than 3.6 volts
+GND   -> GND
+
+*/
+
+
+#include <Wire.h>
+
+#include "LUFAConfig.h"
+#include <LUFA.h>
+#include "Joystick.h"
+#define BOUNCE_WITH_PROMPT_DETECTION
+#include <Bounce2.h>
+#include <SPI.h>
+#include <NRFLite.h>
+#include "Nintendo.h"
+
+
+
+#define MILLIDEBOUNCE 1 //Debounce time in milliseconds
+#define pinOBLED 21  //Onboard LED pin
+
+
+bool buttonStartBefore;
+bool buttonSelectBefore;
+byte buttonStatus[15];
+
+/*
+  0x4000,
+  0x8000,
+#define CAPTURE_MASK_ON 0x2000
+#define R3_MASK_ON 0x800
+#define L3_MASK_ON 0x400
+ */
+#define DPAD_UP_MASK_ON 0x00
+#define DPAD_UPRIGHT_MASK_ON 0x01
+#define DPAD_RIGHT_MASK_ON 0x02
+#define DPAD_DOWNRIGHT_MASK_ON 0x03
+#define DPAD_DOWN_MASK_ON 0x04
+#define DPAD_DOWNLEFT_MASK_ON 0x05
+#define DPAD_LEFT_MASK_ON 0x06
+#define DPAD_UPLEFT_MASK_ON 0x07
+#define DPAD_NOTHING_MASK_ON 0x08
+#define A_MASK_ON 0x04
+#define B_MASK_ON 0x02
+#define X_MASK_ON 0x08
+#define Y_MASK_ON 0x01
+#define LB_MASK_ON 0x10
+#define RB_MASK_ON 0x20
+#define ZL_MASK_ON 0x40
+#define ZR_MASK_ON 0x80
+#define START_MASK_ON 0x200
+#define SELECT_MASK_ON 0x100
+#define HOME_MASK_ON 0x1000
+
+#define BUTTONUP 0
+#define BUTTONDOWN 1
+#define BUTTONLEFT 2
+#define BUTTONRIGHT 3
+#define BUTTONA 4
+#define BUTTONB 5
+#define BUTTONX 6
+#define BUTTONY 7
+#define BUTTONLB 8
+#define BUTTONRB 9
+#define BUTTONLT 10
+#define BUTTONRT 11
+#define BUTTONSTART 12
+#define BUTTONSELECT 13
+#define BUTTONHOME 14
+
+Bounce joystickUP = Bounce();
+Bounce joystickDOWN = Bounce();
+Bounce joystickLEFT = Bounce();
+Bounce joystickRIGHT = Bounce();
+Bounce buttonA = Bounce();
+Bounce buttonB = Bounce();
+Bounce buttonX = Bounce();
+Bounce buttonY = Bounce();
+Bounce buttonLB = Bounce();
+Bounce buttonRB = Bounce();
+Bounce buttonLT = Bounce();
+Bounce buttonRT = Bounce();
+Bounce buttonSTART = Bounce();
+Bounce buttonSELECT = Bounce();
+Bounce buttonHOME = Bounce();
+
+
+const static uint8_t RADIO_ID = 0;       // Our radio's id.  The transmitter will send to this id.
+const static uint8_t PIN_RADIO_CE = 5;
+const static uint8_t PIN_RADIO_CSN = 4;
+int a; 
+int b; 
+int x; 
+int y; 
+int jUp;
+int jDown;
+
+struct RadioPacket // Any packet up to 32 bytes can be sent.
+{
+    uint8_t FromRadioId;
+    uint32_t OnTimeMillis;
+    uint32_t FailedTxCount;
+    Gamecube_Report_t report;
+};
+
+NRFLite _radio;
+RadioPacket _radioData;
+
+typedef enum {
+  ANALOG_MODE,
+  DIGITAL,
+  SMASH
+} State_t;
+State_t state = DIGITAL;
+
+void checkModeChange(){
+   if (false){
+    if(buttonStartBefore == 0 && buttonSelectBefore ==0){
+        switch(state)
+        {
+           case DIGITAL:
+              state=ANALOG_MODE;
+           break;
+
+           case ANALOG_MODE:
+              state=SMASH;
+           break;
+
+           case SMASH:
+              state=DIGITAL;
+           break;
+        }
+        buttonStartBefore = 1;
+        buttonSelectBefore = 1;
+    }
+  }
+  else {buttonSelectBefore = 0;buttonStartBefore = 0;}
+}
+void setupPins(){
+    
+    pinMode(pinOBLED, OUTPUT);  
+    //Set the LED to low to make sure it is off
+    digitalWrite(pinOBLED, HIGH);
+}
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+  buttonStartBefore = false;
+  buttonSelectBefore = false;
+  setupPins();
+  SetupHardware();
+  GlobalInterruptEnable();
+  
+    if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+    {
+        
+        while (1); // Wait here forever.
+    }
+}
+
+
+void loop() {
+    buttonRead();
+    checkModeChange();
+    processButtons();
+    HID_Task();
+    // We also need to run the main USB management task.
+    USB_USBTask();
+    while (_radio.hasData())
+    {
+        _radio.readData(&_radioData); // Note how '&' must be placed in front of the variable name.
+      
+        String msg = "Radio ";
+        msg += _radioData.FromRadioId;
+        msg += ", ";
+        msg += _radioData.OnTimeMillis;
+        msg += " ms, ";
+        msg += _radioData.FailedTxCount;
+        msg += " Failed TX, ";
+        
+        
+        if(a == _radioData.report.y){
+           buttonStatus[BUTTONLT] = true;
+           buttonStatus[BUTTONRT] = true;
+        }
+        else{
+          buttonStatus[BUTTONLT] = false;
+          buttonStatus[BUTTONRT] = false;
+        }
+       
+        int x = _radioData.report.x; 
+        int y = _radioData.report.y; 
+        int jUp;
+        int jDown;
+
+    }
+}
+
+// function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent() {
+  int a = _radioData.report.a;
+  Wire.write(a); // respond with message of 6 bytes
+  // as expected by master
+}
+
+
+void buttonRead()
+{
+
+  if(_radioData.report.a == 1){buttonStatus[BUTTONA] = true;}
+  else{buttonStatus[BUTTONA] = false;}  
+  if( _radioData.report.b == 1){buttonStatus[BUTTONB] = true;}
+  else{buttonStatus[BUTTONB] = false;}   
+  if(_radioData.report.y == 1){buttonStatus[BUTTONY] = true;}
+  else{buttonStatus[BUTTONY] = false;}  
+  if( _radioData.report.x == 1){buttonStatus[BUTTONX] = true;}
+  else{buttonStatus[BUTTONX] = false;}   
+  if( _radioData.report.right > 100){buttonStatus[BUTTONRT] = true;}
+  else{buttonStatus[BUTTONRT] = false;} 
+  if( _radioData.report.left > 100){buttonStatus[BUTTONLT] = true;}
+  else{buttonStatus[BUTTONLT] = false;} 
+  if( _radioData.report.z == 1){buttonStatus[BUTTONRB] = true;}
+  else{buttonStatus[BUTTONRB] = false;}   
+  if( _radioData.report.dup == 1){buttonStatus[BUTTONUP] = true;}
+  else{buttonStatus[BUTTONUP] = false;} 
+  if( _radioData.report.ddown == 1){buttonStatus[BUTTONDOWN] = true;}
+  else{buttonStatus[BUTTONDOWN] = false;} 
+  if( _radioData.report.dleft == 1){buttonStatus[BUTTONLEFT] = true;}
+  else{buttonStatus[BUTTONLEFT] = false;} 
+  if( _radioData.report.dright == 1){buttonStatus[BUTTONRIGHT] = true;}
+  else{buttonStatus[BUTTONRIGHT] = false;} 
+  if( _radioData.report.start == 1){buttonStatus[BUTTONSTART] = true;}
+  else{buttonStatus[BUTTONSTART] = false;} 
+
+
+
+  //if (joystickUP.update()) {buttonStatus[BUTTONUP] = joystickUP.fell();}
+  //if (joystickDOWN.update()) {buttonStatus[BUTTONDOWN] = joystickDOWN.fell();}
+  //if (joystickLEFT.update()) {buttonStatus[BUTTONLEFT] = joystickLEFT.fell();}
+  //if (joystickRIGHT.update()) {buttonStatus[BUTTONRIGHT] = joystickRIGHT.fell();}
+  //if (buttonA.update()) {buttonStatus[BUTTONA] = buttonA.fell();}
+  //if (buttonB.update()) {buttonStatus[BUTTONB] = buttonB.fell();}
+  //if (buttonX.update()) {buttonStatus[BUTTONX] = buttonX.fell();}
+  //if (buttonY.update()) {buttonStatus[BUTTONY] = buttonY.fell();}
+  if (buttonLB.update()) {buttonStatus[BUTTONLB] = buttonLB.fell();}
+  //if (buttonRB.update()) {buttonStatus[BUTTONRB] = buttonRB.fell();}
+  //if (buttonLT.update()) {buttonStatus[BUTTONLT] = buttonLT.fell();}
+  //if (buttonRT.update()) {buttonStatus[BUTTONRT] = buttonRT.fell();}
+  //if (buttonSTART.update()) {buttonStatus[BUTTONSTART] = buttonSTART.fell();}
+  if (buttonSELECT.update()) {buttonStatus[BUTTONSELECT] = buttonSELECT.fell();}
+  if (buttonHOME.update()) {buttonStatus[BUTTONHOME] = buttonHOME.fell();}
+}
+
+
+void processDPAD(){
+    ReportData.LY = 255 - _radioData.report.yAxis;
+    ReportData.LX = _radioData.report.xAxis;
+    ReportData.RX = _radioData.report.cxAxis;
+    ReportData.RY = 255 - _radioData.report.cyAxis;
+}
+void processLANALOG(){
+    ReportData.HAT = DPAD_NOTHING_MASK_ON;
+    ReportData.RX = 128;
+    ReportData.RY = 128;
+
+    if ((buttonStatus[BUTTONUP]) && (buttonStatus[BUTTONRIGHT])){ReportData.LY = 0;ReportData.LX = 255;}
+    else if ((buttonStatus[BUTTONDOWN]) && (buttonStatus[BUTTONRIGHT])) {ReportData.LY = 255;ReportData.LX = 255;}
+    else if ((buttonStatus[BUTTONDOWN]) && (buttonStatus[BUTTONLEFT])) {ReportData.LY = 255;ReportData.LX = 0;}
+    else if ((buttonStatus[BUTTONUP]) && (buttonStatus[BUTTONLEFT])){ReportData.LY = 0;ReportData.LX = 0;}
+    else if (buttonStatus[BUTTONUP]) {ReportData.LY = 0;ReportData.LX = 128;}
+    else if (buttonStatus[BUTTONDOWN]) {ReportData.LY = 255;ReportData.LX = 128;}
+    else if (buttonStatus[BUTTONLEFT]) {ReportData.LX = 0;ReportData.LY = 128;}
+    else if (buttonStatus[BUTTONRIGHT]) {ReportData.LX = 255;ReportData.LY = 128;}
+    else {ReportData.LX = 128;ReportData.LY = 128;}
+}
+void processLANALOGSmash(){
+    ReportData.HAT = DPAD_NOTHING_MASK_ON;
+    ReportData.RX = 128;
+    ReportData.RY = 128;
+    
+    if ((buttonStatus[BUTTONUP]) && (buttonStatus[BUTTONRIGHT])){ReportData.LY = 64;ReportData.LX = 192;}
+    else if ((buttonStatus[BUTTONDOWN]) && (buttonStatus[BUTTONRIGHT])) {ReportData.LY = 192;ReportData.LX = 192;}
+    else if ((buttonStatus[BUTTONDOWN]) && (buttonStatus[BUTTONLEFT])) {ReportData.LY = 192;ReportData.LX = 64;}  
+    else if ((buttonStatus[BUTTONUP]) && (buttonStatus[BUTTONLEFT])){ReportData.LY = 64;ReportData.LX = 64;}
+    else if (buttonStatus[BUTTONUP]) {ReportData.LY = 64;ReportData.LX = 128;}
+    else if (buttonStatus[BUTTONDOWN]) {ReportData.LY = 192;ReportData.LX = 128;}
+    else if (buttonStatus[BUTTONLEFT]) {ReportData.LX = 64;ReportData.LY = 128;}
+    else if (buttonStatus[BUTTONRIGHT]) {ReportData.LX = 192;ReportData.LY = 128;}
+    else{ReportData.LX = 128;ReportData.LY = 128;}
+}
+void processRANALOG(){
+    ReportData.HAT = 0x08;
+    ReportData.LX = 128;
+    ReportData.LY = 128;
+    
+    if ((buttonStatus[BUTTONUP]) && (buttonStatus[BUTTONRIGHT])){ReportData.RY = 0;ReportData.RX = 255;}
+    else if ((buttonStatus[BUTTONUP]) && (buttonStatus[BUTTONLEFT])){ReportData.RY = 0;ReportData.RX = 0;}
+    else if ((buttonStatus[BUTTONDOWN]) && (buttonStatus[BUTTONRIGHT])) {ReportData.RY = 255;ReportData.RX = 255;}
+    else if ((buttonStatus[BUTTONDOWN]) && (buttonStatus[BUTTONLEFT])) {ReportData.RY = 255;ReportData.RX = 0;}
+    else if (buttonStatus[BUTTONUP]) {ReportData.RY = 0;ReportData.RX = 128;}
+    else if (buttonStatus[BUTTONDOWN]) {ReportData.RY = 255;ReportData.RX = 128;}
+    else if (buttonStatus[BUTTONLEFT]) {ReportData.RX = 0;ReportData.RY = 128;}
+    else if (buttonStatus[BUTTONRIGHT]) {ReportData.RX = 255;ReportData.RY = 128;}
+    else {ReportData.RX = 128;ReportData.RY = 128;}
+    
+}
+void processButtons(){
+  //state gets set with checkModeChange
+  switch (state)
+  {
+    case DIGITAL:
+        processDPAD();
+        buttonProcessing();
+    break;
+
+    case ANALOG_MODE:   
+       if(buttonStatus[BUTTONLT]){processRANALOG();}
+       else{processLANALOG();}
+       buttonProcessing();
+    break;
+
+    case SMASH:
+       if(buttonStatus[BUTTONB]){processLANALOGSmash();}
+       else{processLANALOG();}
+       buttonProcessingSmash();
+    break;
+  }
+}
+void buttonProcessing(){
+  if (buttonStatus[BUTTONA]) {ReportData.Button |= A_MASK_ON;}
+  if (buttonStatus[BUTTONB]) {ReportData.Button |= B_MASK_ON;}
+  if (buttonStatus[BUTTONX]) {ReportData.Button |= X_MASK_ON;}
+  if (buttonStatus[BUTTONY]) {ReportData.Button |= Y_MASK_ON;}
+  if (buttonStatus[BUTTONLB]) {ReportData.Button |= LB_MASK_ON;}
+  if (buttonStatus[BUTTONRB]) {ReportData.Button |= RB_MASK_ON;}
+  if (buttonStatus[BUTTONLT]) {ReportData.Button |= ZL_MASK_ON;}
+  if (buttonStatus[BUTTONRT]) {ReportData.Button |= ZR_MASK_ON;}
+  if (buttonStatus[BUTTONSTART]){ReportData.Button |= START_MASK_ON;}
+  if (buttonStatus[BUTTONSELECT]){ReportData.Button |= SELECT_MASK_ON;}
+  if (buttonStatus[BUTTONHOME]){ReportData.Button |= HOME_MASK_ON;}
+}
+void buttonProcessingSmash(){
+  if (buttonStatus[BUTTONA]) {ReportData.Button |= X_MASK_ON;}
+  if (buttonStatus[BUTTONB]) {}
+  if (buttonStatus[BUTTONX]) {ReportData.Button |= A_MASK_ON;}
+  if (buttonStatus[BUTTONY]) {ReportData.Button |= B_MASK_ON;}
+  if (buttonStatus[BUTTONLB]) {ReportData.Button |= LB_MASK_ON;}
+  if (buttonStatus[BUTTONRB]) {ReportData.Button |= ZR_MASK_ON;}
+  if (buttonStatus[BUTTONLT]) {ReportData.Button |= ZL_MASK_ON;}
+  if (buttonStatus[BUTTONRT]) {ReportData.Button |= RB_MASK_ON;}
+  if (buttonStatus[BUTTONSTART]){ReportData.Button |= START_MASK_ON;}
+  if (buttonStatus[BUTTONSELECT]){ReportData.Button |= SELECT_MASK_ON;}
+  if (buttonStatus[BUTTONHOME]){ReportData.Button |= HOME_MASK_ON;}
+}
